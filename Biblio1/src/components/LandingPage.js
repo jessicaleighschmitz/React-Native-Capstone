2c612e2e72735e32017c12b2e7f630917774aa86
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-07-13 16:23:14 -0700

    Add a routing button to the landing page

--- Biblio1/src/components/LandingPage.js
+++ Biblio1/src/components/LandingPage.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, TouchableHighlight, Button, Text, Image, View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-function LandingPage () {
-  return (
-    <View>
-      <Text>Landing</Text>
-   </View>
-  );
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Landing Page</Text>
+        <Button
+          title="Go to Bookshelf"
+          onPress={() => this.props.navigation.navigate('Bookshelf')}
+        />
+      </View>
+    );
+  }
 }
-
 export default LandingPage;
