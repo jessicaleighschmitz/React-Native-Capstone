93f87d53b1a8aa95a9660cc537675820bf637d8a
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-07-13 11:55:50 -0700

    Add Landing Page component

---
+++ Biblio1/src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+function LandingPage () {
+  return (
+    <View>
+      <Text>Landing</Text>
+   </View>
+  );
+}
+
+export default LandingPage;
