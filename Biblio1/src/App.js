bdbe1939438ba318ca124118c1a4edb2c93482e8
Jessica Schmitz <schmitz.jessical@gmail.com>
2018-07-13 15:39:59 -0700

    Begin adding native navigation

--- Biblio1/src/App.js
+++ Biblio1/src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LandingPage from './components/LandingPage';
-import BookShelf from './components/BookShelf';
+// import BookShelf from './components/BookShelf';
+import {createStackNavigator, } from 'react-navigation';
 
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <LandingPage />
-        <BookShelf />
-      </View>
-    );
+    return <RootStack />
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
+
+const RootStack = createStackNavigator({
+  Landing: LandingPage,
+  BookShelf: BookShelf,
+ },
+ {
+   initialRouteName: 'LandingPage',
+ }
+);
+
+
+export default App;
